ae9c3c0758d80acbd5b58a51c1836c591ce48e69
Renato Leme <rntreisleme@gmail.com>
2021-07-20 17:05:03 -0300

    animation

--- js/tracktatus.js
+++ js/tracktatus.js
@@ -18,15 +18,9 @@ genTlp = () => {
 
             let content = data.children[random].content.en + "<p>(TLP, " + (random + 1) + ")</p>";
             
-            document.querySelector('p.animation').classList.toggle('active');
             document.getElementById("tlp").innerHTML = content;
             
             
-        })
-        .finally(res => {
-            setTimeout(function(){
-                 document.querySelector('p.animation').classList.toggle('active');
-            }, 2000);
         });
 }
 
