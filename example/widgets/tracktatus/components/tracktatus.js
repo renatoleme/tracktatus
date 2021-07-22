2b00555796ab9406d5810c99c806d0508c0aee94
Renato Leme <rntreisleme@gmail.com>
2021-07-22 19:12:43 -0300

    new features

---
+++ example/widgets/tracktatus/components/tracktatus.js
@@ -0,0 +1,133 @@
+app.component('tracktatus', {
+    data() {
+        return {
+            tlp_en : "",
+            tlp_de : "",
+            tlp_lang: "de",
+            refresh: false,
+            tlpBox: {
+                width: '500px',
+                marginTop: 10,
+                marginLeft: 10,
+                padding: '4px',
+                background: 'black',
+                color: 'white',
+                overflow: 'hidden',
+                boxShadow: '4px 4px 16px  black',
+                borderRadius: '4px',
+                display: 'flex',
+                flexDirection: 'column'
+            },
+            tlpRefresh: {
+                marginLeft: 'auto'
+            },
+            tlpSelectors: {
+                display: 'flex',
+                justifyContent: 'flex-start'
+            },
+            tlpContent: {
+                padding: '8px',
+                fontSize: '20px',
+                color: 'white',
+                textAlign: 'center'
+            },
+            tlpCredits: {
+                marginLeft: 'auto',
+                padding: '8px',
+                textDecoration: 'none',
+                color: 'gray'
+            }
+        }
+    },
+    template:
+    `<div :style="tlpBox" @mousedown="moveStart($event)" ref="tbox">
+            <div :style="tlpSelectors">
+                <button class="button-lang" v-on:click="changeLang('de')">de</button>
+                <button class="button-lang" v-on:click="changeLang('en')">en</button>
+                <div :style="tlpRefresh">
+                    <button v-on:click="getTlp" class="button-refresh"><i class="fas fa-sync fa-2x"></i></button>
+                </div>
+            </div>
+           <div :style="tlpContent">
+             <transition name="tlp">
+             <div v-if="refresh">
+                  <p v-if="tlp_lang === 'de'" v-html="tlp_de"></p>
+                  <p v-else v-html="tlp_en"></p>
+             </div>
+             <div v-else>
+                  <p v-if="tlp_lang === 'de'" v-html="tlp_de"></p>
+                  <p v-else v-html="tlp_en"></p>
+             </div>
+             </transition>
+           </div>
+
+           <a :style="tlpCredits" href="https://github.com/renatoleme/tracktatus"><i>tracktatus.js</i></a>
+         </div>`,
+    mounted() {
+        this.refresh = !this.refresh;
+
+    },
+    created: function () {
+        this.getTlp();
+    },
+    methods: {
+        moveStart(event) {
+            if (event.toElement.nodeName === "DIV") {
+                const info = { diffY : event.clientY - this.tlpBox.marginTop, diffX : event.clientX - this.tlpBox.marginLeft}
+                this.$emit('move-start', info)
+            }
+        },
+        moveWindow(event, diffX, diffY) {
+            this.tlpBox.marginTop = event.clientY - diffY;
+            this.tlpBox.marginLeft = event.clientX - diffX;
+        },
+        changeLang(lang) {
+            this.tlp_lang = lang;
+        },
+        getRandomInt (min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        },
+        goDeep(data) {
+
+            if (data.children.length !== 0) {
+                const index = this.getRandomInt(0, Object.keys(data.children).length);
+                if (index === 0) {
+                    const content_de = data.content.de + "<br/>(TLP, " + data.key + ")";
+                    const content_en = data.content.en + "<br/>(TLP, " + data.key + ")";
+                    this.tlp_de = content_de;
+                    this.tlp_en = content_en;
+                }
+                this.goDeep(data.children[index])
+            }
+            else {
+                const content_de = data.content.de + "<br/>(TLP, " + data.key + ")";
+                const content_en = data.content.en + "<br/>(TLP, " + data.key + ")";
+                this.tlp_de = content_de;
+                this.tlp_en = content_en;
+            }
+            
+        },
+        getTlp() {
+
+            this.refresh = !this.refresh;
+            
+            const index = this.getRandomInt(0, 7);
+
+            fetch('widgets/tracktatus/data/tractatus.json')
+                .then(response => response.json())
+                .then(json => {
+
+                    const data = json;
+
+                    const content_de = data.children[index].content.de + "<p>(TLP, " + (index + 1) + ")</p>";
+                    const content_en = data.children[index].content.en + "<p>(TLP, " + (index + 1) + ")</p>";
+                    
+                    this.goDeep(data.children[index])
+
+
+                });
+        }
+    }
+})
--- main.js
+++ main.js
@@ -1,119 +1,33 @@
-const Tracktatus = {
+const app = Vue.createApp({
     data() {
         return {
-            tlp_en : "",
-            tlp_de : "",
-            tlp_lang: "de",
-            refresh: false,
-            tlpBox: {
-                padding: '4px',
-                background: 'black',
-                color: 'white',
-                overflow: 'hidden',
-                boxShadow: '4px 4px 16px  black',
-                borderRadius: '4px',
-                display: 'flex',
-                flexDirection: 'column'
-            },
-            tlpRefresh: {
-                marginLeft: 'auto'
-            },
-            tlpSelectors: {
-                display: 'flex',
-                justifyContent: 'flex-start'
-            },
-            tlpContent: {
-                padding: '8px',
-                fontSize: '20px',
-                color: 'white'
-            },
-            tlpCredits: {
-                marginLeft: 'auto',
-                padding: '8px',
-                textDecoration: 'none',
-                color: 'gray'
-            }
+            isWindowMoving: false,
+            diffX: 0,
+            diffY: 0
         }
     },
     template:
-        `<div :style="tlpBox">
-            <div :style="tlpSelectors">
-                <button class="button-lang" v-on:click="changeLang('de')">de</button>
-                <button class="button-lang" v-on:click="changeLang('en')">en</button>
-                <div :style="tlpRefresh">
-                    <button v-on:click="getTlp" class="button-refresh"><i class="fas fa-sync fa-2x"></i></button>
-                </div>
-            </div>
-           <div :style="tlpContent">
-             <transition name="tlp">
-             <div v-if="refresh">
-                  <p v-if="tlp_lang === 'de'" v-html="tlp_de"></p>
-                  <p v-else v-html="tlp_en"></p>
-             </div>
-             <div v-else>
-                  <p v-if="tlp_lang === 'de'" v-html="tlp_de"></p>
-                  <p v-else v-html="tlp_en"></p>
-             </div>
-             </transition>
-           </div>
+`
+            <div class="container" @mousemove="mouseMove($event)" @mouseup="moveEnd">
+        <h1>Tracktatus.js</h1>
 
-           <a :style="tlpCredits" href="https://github.com/renatoleme/tracktatus"><i>tracktatus.js</i></a>
-         </div>`,
-    mounted() {
-            this.refresh = !this.refresh;
+     <tracktatus class="tlp-widget" ref="tbox" v-on:move-start="moveStart"/>
 
-    },
-    created: function () {
-        this.getTlp();
-    },
+</div>
+`,
+    
     methods: {
-        changeLang(lang) {
-            this.tlp_lang = lang;
+        moveEnd() {
+            this.isWindowMoving = false;
         },
-        getRandomInt (min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min)) + min;
+        mouseMove(event) {
+            if (this.isWindowMoving)
+                this.$refs.tbox.moveWindow(event, this.diffX, this.diffY);
         },
-        goDeep(data) {
-
-            if (data.children.length !== 0) {
-                const index = this.getRandomInt(0, Object.keys(data.children).length);
-                if (index === 0) {
-                    const content_de = data.content.de + "<br/>(TLP, " + data.key + ")";
-                    const content_en = data.content.en + "<br/>(TLP, " + data.key + ")";
-                    this.tlp_de = content_de;
-                    this.tlp_en = content_en;
-                }
-                this.goDeep(data.children[index])
-            }
-            else {
-                const content_de = data.content.de + "<br/>(TLP, " + data.key + ")";
-                const content_en = data.content.en + "<br/>(TLP, " + data.key + ")";
-                this.tlp_de = content_de;
-                this.tlp_en = content_en;
-            }
-            
-        },
-        getTlp() {
-
-            this.refresh = !this.refresh;
-            
-            const index = this.getRandomInt(0, 7);
-
-            fetch('widgets/tracktatus/data/tractatus.json')
-                .then(response => response.json())
-                .then(json => {
-
-                    const data = json;
-
-                    const content_de = data.children[index].content.de + "<p>(TLP, " + (index + 1) + ")</p>";
-                    const content_en = data.children[index].content.en + "<p>(TLP, " + (index + 1) + ")</p>";
-                    
-                    this.goDeep(data.children[index])
-
-
-                });
+        moveStart(info) {
+            this.diffX = info.diffX;
+            this.diffY = info.diffY;
+            this.isWindowMoving = true
         }
     }
-}
+})
